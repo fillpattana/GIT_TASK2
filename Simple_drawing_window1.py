cd1c629185d6b396eb9b205bc05537cb96b8412c
Zhengiskan <64011433@kmitl.ac.th>
2023-02-16 17:05:51 +0700

    1.1

--- Simple_drawing_window1.py
+++ Simple_drawing_window1.py
@@ -3,7 +3,8 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
-class Simple_drawing_window1(QWidget):
+
+class Simple_drawing_window(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Github Drawing")
@@ -25,11 +26,37 @@ class Simple_drawing_window1(QWidget):
         
         p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
-        
+
+
+class Simple_drawing_window1(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Github Drawing1")
+        self.man = QPixmap("images/chinese man.png")
+        self.ice = QPixmap("images/bing chilling.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
+        p.drawPolygon([QPoint(50, 200), QPoint(100, 110), QPoint(130, 100), QPoint(100, 150)])
+
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(255, 127, 0))
+        p.drawPie(50, 150, 100, 100, 0, 180 * 6)
+
+        p.drawPolygon([QPoint(50, 200), QPoint(150, 200), QPoint(100, 400)])
+
+        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
+        p.end()
+
+
 def main():
     app = QApplication(sys.argv)
     
-    w = Simple_drawing_window1()
+    w = Simple_drawing_window()
     w.show()
     
     return app.exec()
