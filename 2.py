629d0a0e83ce1ee4d52db2aaae0bc85fcb9bc653
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2023-02-16 18:02:23 +0700

    2 update

--- 2.py
+++ 2.py
@@ -3,11 +3,70 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QPushButton,
+    QSizePolicy, QWidget)
 
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(400, 300)
+        self.frame = QFrame(Form)
+        self.frame.setObjectName(u"frame")
+        self.frame.setGeometry(QRect(80, 30, 221, 221))
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+    
+        
+        
+        self.label = QLabel(self.frame)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(0, 0, 221, 171))
+        
+        canvas = QPixmap(400, 300)
+        canvas.fill(QColor('white'))
+        self.label.setPixmap(canvas)
+        
+        self.pushButton = QPushButton(self.frame)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(10, 190, 211, 32))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.pushButton.setText(QCoreApplication.translate("Form", u"Clear", None))
+        
+    def mouseMoveEvent(self, e):
+        canvas = self.label.pixmap()
+        painter = QPainter(canvas)
+        painter.drawPoint(e.x(), e.y())
+        painter.end()
+        self.label.setPixmap(canvas)
+        
+class Calculator(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+    
+        
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        
+        
 class Simple_freehand_drawing(QMainWindow):
     def __init__(self):
         super().__init__()
-        # self.setWindowTitle("Simple Freehand Drawing")
+        self.setWindowTitle("Simple Freehand Drawing")
         # self.window = QWidget()
         # self.layout = QVBoxLayout()
         #
@@ -20,11 +79,20 @@ class Simple_freehand_drawing(QMainWindow):
         # self.layout.addWidget(self.rec)
         # self.layout.addWidget(self.clear)
         # self.window.setLayout(self.layout)
+        self.win = QWidget()
+        self.vbox = QVBoxLayout()
         self.rec = QLabel()
         canvas = QPixmap(400, 300)
         canvas.fill(QColor('white'))
         self.rec.setPixmap(canvas)
-        self.setCentralWidget(self.rec)
+        self.clear = QPushButton("clear")
+        self.vbox.addWidget(self.rec)
+        self.vbox.addWidget(self.clear)
+        self.win.setLayout(self.vbox)
+        
+        
+        
+        
 
     def mouseMoveEvent(self, e):
         canvas = self.rec.pixmap()
@@ -36,7 +104,7 @@ class Simple_freehand_drawing(QMainWindow):
 
 def main():
     app = QApplication(sys.argv)
-    w = Simple_freehand_drawing()
+    w = Calculator()
     w.show()
 
     return app.exec()
---
+++ sjjs.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'designerKvgLVv.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QPushButton,
+    QSizePolicy, QWidget)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(400, 300)
+        self.frame = QFrame(Form)
+        self.frame.setObjectName(u"frame")
+        self.frame.setGeometry(QRect(80, 30, 221, 221))
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.label = QLabel(self.frame)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(0, 0, 221, 171))
+        self.pushButton = QPushButton(self.frame)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(10, 190, 211, 32))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.label.setText(QCoreApplication.translate("Form", u"TextLabel", None))
+        self.pushButton.setText(QCoreApplication.translate("Form", u"Clear", None))
+    # retranslateUi
+
