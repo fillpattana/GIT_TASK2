07a66b945d0491d372f7f5f2fc87ecdc702748a6
Zhengiskan <64011433@kmitl.ac.th>
2023-02-16 16:52:18 +0700

    3.1.py

--- h.py
+++ h.py
@@ -1,1 +1,7 @@
-print("hi")
+class Disk(object):
+    def __init__(self, name="", xpos=0, ypos=0, height=20, width=40):
+        self.dname = name
+        self.dxpos = xpos
+        self.dypos = ypos
+        self.dheight = height
+        self.dwidth = width
