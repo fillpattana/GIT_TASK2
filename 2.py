d93d842da1369e30011d5b8a86e89c10552bee26
Zhengiskan <64011433@kmitl.ac.th>
2023-02-16 17:39:09 +0700

    hi

--- 2.py
+++ 2.py
@@ -7,19 +7,24 @@ from PySide6.QtGui import *
 class Simple_freehand_drawing(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Simple Freehand Drawing")
-        self.window = QWidget()
-        self.layout = QVBoxLayout()
-
+        # self.setWindowTitle("Simple Freehand Drawing")
+        # self.window = QWidget()
+        # self.layout = QVBoxLayout()
+        #
+        # self.rec = QLabel()
+        # canvas = QPixmap(400, 300)
+        # canvas.fill(QColor('white'))
+        # self.rec.setPixmap(canvas)
+        #
+        # self.clear = QPushButton("clear")
+        # self.layout.addWidget(self.rec)
+        # self.layout.addWidget(self.clear)
+        # self.window.setLayout(self.layout)
         self.rec = QLabel()
         canvas = QPixmap(400, 300)
         canvas.fill(QColor('white'))
         self.rec.setPixmap(canvas)
-
-        self.clear = QPushButton("clear")
-        self.layout.addWidget(self.rec)
-        self.layout.addWidget(self.clear)
-        self.window.setLayout(self.layout)
+        self.setCentralWidget(self.rec)
 
     def mouseMoveEvent(self, e):
         canvas = self.rec.pixmap()
